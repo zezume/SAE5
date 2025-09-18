34bc35d2c9bc7cfd82dfab752cc1700f56556228
Jean-Baptiste Guérin <Jean-Baptiste.Guerin@uphf.fr>
2025-09-18 10:41:20 +0200

    accès à la caméra

--- jpp/app/src/main/java/com/example/jpp/MainActivity.java
+++ jpp/app/src/main/java/com/example/jpp/MainActivity.java
@@ -1,24 +1,57 @@
 package com.example.jpp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST_CODE = 100;
+
+    // Gestion de la permission
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    openCamera();
+                } else {
+                    Toast.makeText(this, "Permission caméra refusée", Toast.LENGTH_SHORT).show();
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnOpenCamera = findViewById(R.id.btnOpenCamera);
+
+        btnOpenCamera.setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                // Demande la permission
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+            }
         });
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+        } else {
+            Toast.makeText(this, "Aucune app caméra trouvée", Toast.LENGTH_SHORT).show();
+        }
     }
 }
