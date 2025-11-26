8eeeb0ee360bc0cfdfbf140d1b4f7f5ef9a45552
Jean-Baptiste Guérin <Jean-Baptiste.Guerin@uphf.fr>
2025-11-26 14:38:36 +0100

    changement ui menu

--- jpp/app/src/main/java/com/example/jpp/MainActivity.java
+++ jpp/app/src/main/java/com/example/jpp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.vuzix.hud.actionmenu.ActionMenuActivity;
@@ -20,10 +22,31 @@ public class MainActivity extends ActionMenuActivity {
         title = findViewById(R.id.tvTitle);
         title.setText("Bienvenue");
     }
-
     @Override
     protected boolean onCreateActionMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // ---- Questionnaire ----
+        MenuItem questionnaireItem = menu.findItem(R.id.action_questionnaire);
+        LinearLayout qLayout = (LinearLayout) questionnaireItem.getActionView();
+        TextView qView = qLayout.findViewById(R.id.menuItemText);
+        ImageView qIcon = qLayout.findViewById(R.id.menuItemIcon);
+
+        qView.setText("Questionnaire");
+        qIcon.setImageResource(R.drawable.img_menu_question);
+
+        // ---- Scan ----
+        MenuItem scanItem = menu.findItem(R.id.action_scan);
+        LinearLayout sLayout = (LinearLayout) scanItem.getActionView();
+        TextView sView = sLayout.findViewById(R.id.menuItemText);
+        ImageView sIcon = sLayout.findViewById(R.id.menuItemIcon);
+
+        sView.setText("Scanner");
+        sIcon.setImageResource(R.drawable.img_menu_qrcode);
+
+        // ---- Contour épais pour item actif (optionnel) ----
+        qView.setBackgroundResource(R.drawable.menu_item_active);
+
         return true;
     }
 
